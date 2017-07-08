99a4241e20f47e4ce4a185c7c248915a5b024ef4
sangeetha <msangeetha100@gmail.com>
2017-07-07 23:17:12 -0700

    Added Utility

--- Mvc 5 Empty Template1/Mvc 5 Empty Template1/Controllers/HomeController.cs
+++ Mvc 5 Empty Template1/Mvc 5 Empty Template1/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 
 namespace Mvc_5_Empty_Template1.Controllers
 {
@@ -9,5 +10,24 @@ namespace Mvc_5_Empty_Template1.Controllers
         {
             return View();
         }
+
+        public bool ChangeStatus(string d1Status)
+        {
+            bool _isOn = Convert.ToBoolean(d1Status);
+            if (_isOn)
+                _isOn = false;
+            return _isOn;
+        }
+
+        public int IncreaseTemp(string currenttemperature, string isIncrease)
+        {
+            int _currentTemp = Convert.ToInt32(currenttemperature);
+            bool _isIncrease = Convert.ToBoolean(isIncrease);
+            if (_isIncrease)
+                return ++_currentTemp;
+            else
+                return --_currentTemp;
+        }
+
     }
 }
---
+++ Mvc 5 Empty Template1/Mvc 5 Empty Template1/Models/Utility.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mvc_5_Empty_Template1.Models
+{
+    public class Utility
+    {
+        public int threshholdmax = 40;
+
+        public int threshholdmin = 16;
+
+        public int maxdeviceusageduration = 24;
+    }
+}
