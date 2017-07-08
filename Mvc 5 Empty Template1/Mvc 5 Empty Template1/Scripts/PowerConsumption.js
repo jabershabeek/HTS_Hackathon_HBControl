c4fd6c73f857fce65ef6ab47d371d6d185104b17
Dhipika <dhipika@gitup.com>
2017-07-08 12:22:34 +0530

    added script

--- Mvc 5 Empty Template1/Mvc 5 Empty Template1/Scripts/PowerConsumption.js
+++ Mvc 5 Empty Template1/Mvc 5 Empty Template1/Scripts/PowerConsumption.js
@@ -1,4 +1,28 @@
 ﻿var objPowerConsumption = new function () {
 
+    //$.ajax({
+    //    type: "POST",
+    //    url: "/webservices/PodcastService.asmx/CreateMarkers",
+    //    data: markers,
+    //    contentType: "application/json; charset=utf-8",
+    //    dataType: "json",
+    //    success: function(data){alert(data);},
+    //    failure: function(errMsg) {
+    //        alert(errMsg);
+    //    }
+
+
+    //$.ajax({
+    //    url: '/test/PersonSubmit',
+    //    type: 'post',
+    //    dataType: 'json',
+    //    success: function (data) {
+    //        $('#target').html(data.msg);
+    //    },
+    //    data: person
+    //});
+
+
+
 
 }
